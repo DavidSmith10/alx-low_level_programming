fbc3e8748b1e23c39c44176c98b0468034aa7726
DavidSmith10 <davesking15@gmail.com>
2022-12-12 07:34:19 -0800

    script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - Entry
  * Return: Always 0
  */
- int main (void)
+ int main(void)
 {
-	puts("\"programming is like building a multi lingual puzzle');
-	return (0)
+	puts("\"programming is like building a multi lingual puzzle");
+	return (0);
 }
