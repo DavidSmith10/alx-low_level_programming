2ce6590dd9eb36a23bd532934275ce8f387ee062
DavidSmith10 <davesking15@gmail.com>
2022-12-12 07:40:13 -0800

    script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0
- */
- int main(void)
+* main - Entry
+* Return: Always 0
+*/
+int main(void)
 {
 	puts("\"programming is like building a multi lingual puzzle");
 	return (0);
