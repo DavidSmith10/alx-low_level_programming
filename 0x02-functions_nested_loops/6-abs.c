c54b7a991ce15b352aedfea34fedcebf42ffde67
DavidSmith10 <davesking15@gmail.com>
2022-12-16 06:18:08 -0800

    script

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _abs -> retuen absolute value of a number
+* @n: parameter
+* Return: absoute value of a number
+*/
+int _abs(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
