3d73304d688f475c32b165e8c5865d432415cf8d
DavidSmith10 <davesking15@gmail.com>
2022-12-16 15:57:08 -0800

    script

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * main - Tests functiom for positive or negative
- * Return: 0
- */
+* main - Tests function for positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
-int i;
+	int i;
 
-i = 0;
-positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-return (0);
-
+	return (0);
 }
