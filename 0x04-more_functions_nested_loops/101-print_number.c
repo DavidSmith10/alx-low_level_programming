6999075b9d73c98e2b141e72e39331390f697624
DavidSmith10 <davesking15@gmail.com>
2022-12-18 05:43:18 -0800

    script

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* print_number - prints int with putchar
-* @n: takes number, hello
-*
-* Return: void
-*/
+ * print_number - prints int with putchar
+ * @n: takes number, hello
+ *
+ * Return: void
+ */
 
 void print_number(int n)
 {
