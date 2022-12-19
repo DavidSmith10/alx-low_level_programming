25c2430149c4f0669dbfe4520e8c135e4238d3d9
DavidSmith10 <davesking15@gmail.com>
2022-12-19 14:50:46 -0800

    script

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by aa new line.
+ * @str: printer to the string to print
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
