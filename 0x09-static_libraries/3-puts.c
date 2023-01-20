d130b406406d06f94354233988bc7c61b49b5c73
DavidSmith10 <davesking15@gmail.com>
2023-01-20 12:27:37 -0800

    update

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,12 +1,14 @@
 #include "main.h"
-#include<stdio.h>
+#include <stdio.h>
+
 /**
-* main - Entry point
-*
+* _puts - print statement
+* @s : inputs value
 * Return: Always 0 (Success)
 */
-int main(void)
+
+void _puts(char *s)
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+
 }
